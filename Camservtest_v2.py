735f0e60d5275c96f878e9a0cda29629015a3faa
TheOnlyFP <TheOnlyFP@users.noreply.github.com>
2018-12-19 13:32:05 +0100

    Remote_car

---
+++ Camservtest_v2.py
@@ -0,0 +1,138 @@
+import cv2
+import numpy as np #Needed for opencv it seems, also used in camcap()
+import socket
+
+def initserv():
+    sendsocketip = '192.168.43.29'
+    sendsocketport = 55555
+
+    sendsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sendsock.bind((sendsocketip, sendsocketport))
+
+    return sendsock
+
+def reconnector(sendsock):
+
+    sendsock.listen()
+    connection, fromadress = sendsock.accept()
+
+    return connection
+
+
+def connector():
+    recsocketip = '192.168.43.113' 
+    recsocketport = 33333
+
+    print("attempting connection to client")
+
+    recsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    recsock.connect((recsocketip, recsocketport))
+
+    return recsock
+
+
+def showframe(connection):
+    data = b""
+    while True:
+        package = connection.recv(16)
+        if not package:
+            package = b""
+            break
+        data += package
+
+    encodedframe = data
+
+    connection.shutdown(socket.SHUT_RDWR)
+    connection.close()
+
+    shapedframe = np.frombuffer(encodedframe, np.uint8).reshape((180,320,3))
+
+    cv2.imshow("ServerframeX", shapedframe)
+
+    if cv2.waitKey(1) == ord('q'):  #Allows for quitting the frame
+        #Was changed back to 1 as it needs it to exist in a timeframe
+        #for it to display the frame and go forward in the code
+        cv2.destroyAllWindows() 
+
+    return shapedframe
+
+def linecalulator(frame, oldlinex):
+
+    width = 160
+    height = 120
+
+    Blackline = cv2.inRange(frame, (0,0,0), (50,50,50))
+
+    img, contours, hierachy = cv2.findContours(Blackline.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    if len(contours) > 0:
+        x,y,w,h = cv2.boundingRect(contours[0])
+
+        linex = int(x+(w/2))
+
+        cv2.line(frame, (linex,0), (linex, height), (255,0,0),3)
+
+    else:
+        print("No contour found")
+        linex=oldlinex
+        oldlinex = linex
+
+    return linex, oldlinex
+
+def PIDcont(linex, cumError, lastError):
+    pval = 1
+    ival = 0.1
+    dval = 1
+    setpoint = 80
+    MaxCorr = 160
+    MinCorr = -160
+    
+    error = setpoint - linex
+    pcorr = pval * error
+
+    # ^ P
+
+    cumError = cumError + error
+    icorr = ival*cumError
+
+    # ^ I
+
+    slope = error - lastError
+    dcorr = slope * dval
+    lastError = error
+
+    # ^ D
+
+    corr = pcorr + icorr + dcorr
+
+    if corr > MaxCorr:
+        corr = MaxCorr
+    if corr < MinCorr:
+        corr = MinCorr
+
+    print("Correction", str(corr))
+
+    return(corr, cumError, lastError)
+
+
+def sendval(recsock, corr):
+
+    recsock.sendall(str(corr).encode('utf-8'))
+    recsock.close()
+
+
+def main():
+    cumError = 0
+    lastError = 0
+    oldlinex = 80
+    sendsock = initserv()
+    while True:
+        connection = reconnector(sendsock) #accept connection on sendock
+        nonbyteframe = showframe(connection) #gey frame over sendsock
+        linex, oldlinex = linecalulator(nonbyteframe, oldlinex) 
+        corr, cumError, lastError = PIDcont(linex, cumError, lastError)
+        recsock = connector()
+        sendval(recsock, corr)
+        print(corr)
+
+main()
---
+++ Camsockettest_v2.py
@@ -0,0 +1,195 @@
+import cv2
+import numpy as np #Needed for opencv it seems, also used in camcap()
+import socket
+import time
+
+cap = cv2.VideoCapture(0)
+width = 160
+height = 120
+ret = cap.set(3,width)
+ret = cap.set(4,height)
+
+GPIO.setmode(GPIO.BCM)
+#inchan_list = [27,23,22] #Insert Anime joke here
+outchan_list = [21,20,26,19,16,13,6,5] #-\\-
+
+forward_list = [21,19,13,6] #All channels that make the wheels go forward
+forward_stop = [20,26,16,5] #Reverse, but also to turn the other direction off
+left_list = [26,16,21,6] #to turn car left / left motors go back
+right_list = [19,13,20,5] # -\\- right
+all_list = [20,19,13,5,21,26,16,6]
+
+#GPIO.setup(inchan_list, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #Sets them up as inputs
+GPIO.setup(outchan_list, GPIO.OUT, initial=GPIO.LOW) #Sets them up as outputs
+
+
+freq= 15
+
+#setup end
+
+
+pwmflf = GPIO.PWM(19,freq)
+pwmblf = GPIO.PWM(13,freq)
+pwmfrf = GPIO.PWM(21,freq)
+pwmbrf = GPIO.PWM(6, freq)
+pwmflb = GPIO.PWM(26,freq)
+pwmblb = GPIO.PWM(16,freq)
+pwmfrb = GPIO.PWM(20,freq)
+pwmbrb = GPIO.PWM(5, freq)
+
+def motorint():
+    pwmflf.start(0)
+    pwmblf.start(0)
+    pwmfrf.start(0)
+    pwmbrf.start(0)
+    pwmflb.start(0)
+    pwmblb.start(0)
+    pwmfrb.start(0)
+    pwmbrb.start(0)
+
+def connector():
+    sendsocketip = '192.168.43.29' 
+    sendsocketport = 55555
+
+    sendsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sendsock.connect((sendsocketip, sendsocketport))
+
+    return sendsock
+
+
+def serverint():
+    recsocketip = '192.168.43.113'
+    recsocketport = 33333
+
+    recsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    recsock.bind((recsocketip, recsocketport))
+
+    return recsock
+
+
+def serverconnector(recsock):
+    print("listening")
+    recsock.listen()
+    connection, fromaddress = recsock.accept()
+
+    return connection
+
+def camcap(sendsock):
+    ret, frame = cap.read(0)
+    if cap.isOpened() == 0:
+        cap.open(0)
+
+    cv2.imshow("FirstframeX", frame)
+
+
+    if cv2.waitKey(1) == ord('q'):  #Allows for quitting the frame
+        #Was changed back to 1 as it needs it to exist in a timeframe
+        #for it to display the frame and go forward in the code
+        cap.release()
+        cv2.destroyAllWindows()
+
+    encodedframe = frame.tostring()
+
+    sendsock.sendall(encodedframe)
+
+    sendsock.shutdown(socket.SHUT_RDWR)
+    sendsock.close()
+
+    nonbyteframe = np.frombuffer(encodedframe, np.uint8)
+
+    shapedframe = nonbyteframe.reshape(180,320,3) 
+
+
+def recval(connection):
+    data = b""
+    while True:
+        package = connection.recv(16)
+        if not package:
+            package = b""
+            break
+        data += package
+
+    connection.shutdown(socket.SHUT_RDWR)
+    connection.close()
+
+    value = data.decode('utf-8')
+
+    print("Decoded value: ", value)
+
+    value = float(value)
+
+    value = int(value)
+
+    return value
+
+def motstate(corr):
+    if corr >= -50 and corr <= 50: #forward
+        allforward(100)
+    elif corr > 30: #right
+        power = int((corr/160)*100)
+        print(power)
+        turnleft(power)
+    elif corr < -30: #Left
+        power = int((corr/-160)*100)
+        print(power)
+        turnright(power)
+
+
+def allforward(dc):
+    pwmflf.ChangeDutyCycle(dc)
+    pwmblf.ChangeDutyCycle(dc)
+    pwmfrf.ChangeDutyCycle(dc)
+    pwmbrf.ChangeDutyCycle(dc)
+    pwmflb.ChangeDutyCycle(0)
+    pwmblb.ChangeDutyCycle(0)
+    pwmfrb.ChangeDutyCycle(0)
+    pwmbrb.ChangeDutyCycle(0)
+
+def backward(dc):
+    pwmflf.ChangeDutyCycle(0)
+    pwmblf.ChangeDutyCycle(0)
+    pwmfrf.ChangeDutyCycle(0)
+    pwmbrf.ChangeDutyCycle(0)
+    pwmflb.ChangeDutyCycle(dc)
+    pwmblb.ChangeDutyCycle(dc)
+    pwmfrb.ChangeDutyCycle(dc)
+    pwmbrb.ChangeDutyCycle(dc)
+
+    
+def turnleft(dc):
+    pwmflb.ChangeDutyCycle(dc)
+    pwmblb.ChangeDutyCycle(dc)
+    pwmfrf.ChangeDutyCycle(dc)
+    pwmbrf.ChangeDutyCycle(dc)
+    pwmfrb.ChangeDutyCycle(0)
+    pwmbrb.ChangeDutyCycle(0)
+    pwmflf.ChangeDutyCycle(0)
+    pwmblf.ChangeDutyCycle(0)
+
+
+def turnright(dc):
+    pwmflb.ChangeDutyCycle(0)
+    pwmblb.ChangeDutyCycle(0)
+    pwmfrf.ChangeDutyCycle(0)
+    pwmbrf.ChangeDutyCycle(0)
+    pwmfrb.ChangeDutyCycle(dc)
+    pwmbrb.ChangeDutyCycle(dc)
+    pwmflf.ChangeDutyCycle(dc)
+    pwmblf.ChangeDutyCycle(dc)
+
+
+def main():
+    motorint()
+    recsock = serverint() #starts server
+    while True:
+        sendsock = connector() #connect to server
+        camcap(sendsock) #take and send frame over sendsock
+        print("camcap sendt")
+        connection = serverconnector(recsock) #connect to recsock
+        value = recval(connection) #get value from recsock
+        PID(value) 
+
+
+main()
+
+GPIO.cleanup()
