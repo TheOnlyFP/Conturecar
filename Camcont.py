032f7639ae3dd530ab81d2d3f8f3b1c76b542406
TheOnlyFP <TheOnlyFP@users.noreply.github.com>
2018-12-22 18:50:03 +0100

    update

--- Camcont.py
+++ Camcont.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 
-# from gpiozero import MCP3008
+from gpiozero import MCP3008
 import RPi.GPIO as GPIO
 from time import sleep 
 import numpy as np #Needed for opencv it seems, also used in camcap()
 import cv2
+import socket
 
 #import ends
 
-# MCP3008(channel=0, clock_pin=11, mosi_pin=10, miso_pin=9, select_pin=8)
+host_ip = '192.168.3.14'
+host_port = 44443
 
-# MCP0=MCP3008(0)
+MCP3008(channel=0, clock_pin=11, mosi_pin=10, miso_pin=9, select_pin=8)
+
+MCP0=MCP3008(0)
 
 GPIO.setmode(GPIO.BCM)
 #inchan_list = [27,23,22] #Insert Anime joke here
@@ -79,59 +83,55 @@ def main():
         cumin = 0
         cumax = 0
         power = 0
-##        allforward(50)
-        #ALL below needs tuning on the numbers as the framesize was changed
+        info_count = 0
+        info_list = []
+        sock = socketconnect(host_ip, host_port)   
         while True:
-            #setpoint = 80
             linex, oldlinex = camcap(oldlinex)
             corr, cumError, lastError = PIDcont(linex, cumError, lastError)
-
-            # Make PID for each side seperatly? 
-            # Speed of left side = Desired speed - Corr
-            # Speed of right side = Desired speed + Corr
-            # If desired speed == 50%
-            # That would give 50% to tune in
-            # Base turning on differential speed-steering
 
             powerleft = 100 - corr
             powerright = 100 + corr
 
             if powerleft >= 100:
                 powerleft = 100
+            elif powerleft < 0:
+                powerleft = 0
 
             if powerright >= 100:
                 powerright = 100
-
-            print(powerleft)
-            print(powerright)
+            elif powerright < 0:
+                powerright = 0
 
             powleft(powerleft)
             powright(powerright)
 
-            # if corr >= -60 and corr <= 60: #forward
-            #     allforward(40)
-            # elif corr > 40: #right
-            #     power = int((corr/160)*100)
-            #     print(power)
-            #     turnleft(power)
-            # elif corr < -40: #Left
-            #     power = int((corr/-160)*100)
-            #     print(power)
-            #     turnright(power)
-            #testfunc()
-        #MCPvalue = checkvalMCP0(MCP0)
-            print("\n")
+
+            info_count += 1
+            if info_count == 30:
+                info_list.append("MCP3008: " + str(checkvalMCP0(MCP0)))
+                info_list.append("Linex :" + str(linex))
+                info_list.append("Powerleft: " + str(powerleft))
+                info_list.append("Powerright: " + str(powerright))
+                sock.sendall(str(info_list).encode('utf-8'))
+                info_count = 0
+                info_list = []
+
     except KeyboardInterrupt:
-        print(power)
         GPIO.cleanup()
 
+def socketconnect(host_ip, host_port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host_ip, host_port))
+    return sock
+
 def PIDcont(linex, cumError, lastError):
-    pval = 3
-    ival = 0.1
-    dval = 2.4
+    pval = 3.5
+    ival = 0.01
+    dval = 4
     setpoint = 80
-    MaxCorr = 200
-    MinCorr = -200
+    MaxCorr = 100
+    MinCorr = -100
     
     error = setpoint - linex
     pcorr = pval * error
@@ -156,8 +156,6 @@ def PIDcont(linex, cumError, lastError):
     if corr < MinCorr:
         corr = MinCorr
 
-    print("Correction", str(corr))
-
     return(corr, cumError, lastError)
     
 
@@ -166,15 +164,9 @@ def camcap(oldlinex):
     if cap.isOpened() == 0:
         cap.open(0)
 
-    Blackline = cv2.inRange(frame, (0,0,0), (120,120,120))
+    Blackline = cv2.inRange(frame, (0,0,0), (110,110,110))
 
     img, contours, hierachy = cv2.findContours(Blackline.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    # kernel = np.ones((3,3), np.uint8)
-
-    # Blackline= cv2.erode(Blackline, kernel, iterations = 3)
-
-    # Blackline= cv2.dilate(Blackline, kernel, iterations = 9)
 
     if len(contours) > 0:
         x,y,w,h = cv2.boundingRect(contours[0])
@@ -183,95 +175,42 @@ def camcap(oldlinex):
 
         cv2.line(frame, (linex,0), (linex, height), (255,0,0),3)
 
+
     else:
         linex=oldlinex
 
-    print(linex)
-    
-    cv2.imshow('frame',frame)  #Shows picture in frame called "frame"
-
-    if cv2.waitKey(1) == ord('q'):  #Allows for quitting the frame
-        #Was changed back to 1 as it needs it to exist in a timeframe
-        #for it to display the frame and go forward in the code
-        cap.release()
-        cv2.destroyAllWindows()
-
     return linex, oldlinex
-
-
-def powleft(dc):
-    if dc >= 0:
-        pwmflb.ChangeDutyCycle(0)
-        pwmblb.ChangeDutyCycle(0)
-        pwmflf.ChangeDutyCycle(dc)
-        pwmblf.ChangeDutyCycle(dc)
-    else:
-        dc = dc*(-1)
-        pwmflb.ChangeDutyCycle(dc)
-        pwmblb.ChangeDutyCycle(dc)
-        pwmflf.ChangeDutyCycle(0)
-        pwmblf.ChangeDutyCycle(0)
-
-
-def powright(dc):
-    if dc >= 0:
-        pwmfrb.ChangeDutyCycle(0)
-        pwmbrb.ChangeDutyCycle(0)
-        pwmfrf.ChangeDutyCycle(dc)
-        pwmbrf.ChangeDutyCycle(dc)
-    else:
-        dc = dc*(-1)
-        pwmfrb.ChangeDutyCycle(dc)
-        pwmbrb.ChangeDutyCycle(dc)
-        pwmfrf.ChangeDutyCycle(0)
-        pwmbrf.ChangeDutyCycle(0)       
-
-
-# def checkvalMCP0(MCP0):
-#     value = MCP0.value
-#     print(value)
-#     value = int(value*10)
-#     return value
-
 
 
 #left_list = [26,16,21,6] #to turn car left
 #right_list = [19,13,20,5] # -\\- right
 
-#mfl = f(19) b(26)
-#mbl = f(13) b(16)
-#mfr = f(21) b(20)
-#mbr = f(6)  b(5)
 
-##motordict= {  #Legacy
-##    "mflf":19,
-##    "mflb":26,
-##    "mblf":13,
-##    "mblb":16,
-##    "mfrf":21,
-##    "mfrb":20,
-##    "mbrf":6,
-##    "mbrb":5,
-##    }
+def powleft(dc):
+    if dc < 0:
+        dc = 0
+    pwmflb.ChangeDutyCycle(0)
+    pwmblb.ChangeDutyCycle(0)
+    pwmflf.ChangeDutyCycle(dc)
+    pwmblf.ChangeDutyCycle(dc)
 
 
-def testfunc(): #Legacy
-    print("Test")
-    GPIO.output(all_list, 0)
-##    iner = input("Please enter motor\n")
-##    GPIO.output(motordict[iner],1)
-##    input()
-##    GPIO.output(motordict[iner],1)
-##    input()
-    input()
-    GPIO.output(right_list,1)
-    input()
-    GPIO.output(right_list,0)
+def powright(dc):
+    if dc < 0:
+        dc = 0
+    pwmfrb.ChangeDutyCycle(0)
+    pwmbrb.ChangeDutyCycle(0)
+    pwmfrf.ChangeDutyCycle(dc)
+    pwmbrf.ChangeDutyCycle(dc)      
+
+
+def checkvalMCP0(MCP0):
+    value = MCP0.value
+    value = (value*10)
+    return value
 
 #function end
 
-
-#Event end
 
 print("start") #Kinda not needed, looks good though
 
---
+++ import socket.py
@@ -0,0 +1,32 @@
+import socket
+
+def main():
+    host_ip = '192.168.3.14'
+    host_port = 44443
+    sock = servercreate(host_ip, host_port)
+    connection = serveracc(sock)
+    print("Connection made \n")
+    pack = ""
+    data = b''
+    while True:
+        data = connection.recv(160)
+        data=data.decode('utf-8')
+        timedata = data[1:-2]
+        datalist = timedata.split(",")
+        print(datalist)
+        if not data:
+            break
+
+
+def servercreate(host_ip, host_port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((host_ip, host_port))
+    return sock
+
+def serveracc(sock):
+    sock.listen()
+    connection, fadd = sock.accept()
+    return connection
+    
+
+main()
